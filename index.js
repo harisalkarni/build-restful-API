2c1a24a2e2b53d579e814be1507126a1c3a3e86d
harisalkarni <haris.alkarni@gmail.com>
2022-07-15 18:34:38 +0700

    2nd update

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 app.get('/api/courses', (req, res) => {
     res.send([1,2,3])
 })
-
+//update something
 app.listen(3000, () => console.log('Listening on port 3000..'))
